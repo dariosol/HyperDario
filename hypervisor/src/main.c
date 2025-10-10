d55d26a9d069aff4b364356a576fb6055441ab94
dariosol <d.soldi89@gmail.com>
2025-10-10 14:49:06 +0200

    explicit check that i am in EL2

--- hypervisor/src/main.c
+++ hypervisor/src/main.c
@@ -1,8 +1,20 @@
 #include "uart.h"
+//mrs %0, CurrentEL moves the value of the system register CurrentEL 
+//into a general-purpose register (and then into the C variable el).
+static inline unsigned int get_current_el(void) {
+    unsigned long el;
+    asm volatile ("mrs %0, CurrentEL" : "=r"(el));
+    return (el >> 2) & 3;
+}
 
 void main(void) {
     uart_init();
-    uart_puts("HyperDario starting at EL2...\n");
+    uart_puts("HyperDario starting...\n");
+
+    unsigned int el = get_current_el();
+    uart_puts("Current EL: ");
+    uart_putc('0' + el);
+    uart_puts("\n");
 
     while (1) {
         uart_puts("Running...\n");
